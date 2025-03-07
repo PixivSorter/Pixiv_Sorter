76281811f7310f471c1edef2b764b669c2cea91e
PixivSorter <pixivsorter@gmail.com>
2025-03-07 16:17:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -66,12 +66,12 @@ class collectIllustInfo():
         elif content == 2:
             file_name = 'R18'
 
-        thumbnail_file = f'/tmp/{self.user_id}/{file_name}/'
+        thumbnail_file = f'/tmp/#{self.user_id}/{file_name}/'
 
         try:
             os.makedirs(thumbnail_file)
         except FileExistsError:
-            shutil.rmtree(f'/tmp/{self.user_id}/{file_name}')
+            shutil.rmtree(f'/tmp/#{self.user_id}/{file_name}')
             os.mkdir(thumbnail_file)
 
         if len(data) <= 15:
@@ -93,7 +93,7 @@ class collectIllustInfo():
             temp_path = old_path.split('/')
             
             temp_path.pop(-1)
-            temp_path.append(f'{i+1}.jpg')
+            temp_path.append(f'{self.user_id}_{self.file_name}_{i+1}.jpg')
 
             new_path = '/'.join(temp_path)
 
