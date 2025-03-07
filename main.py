cca90b672cfef5576b8591c9e590a5a6f9efa59e
PixivSorter <pixivsorter@gmail.com>
2025-03-07 17:00:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ class collectIllustInfo():
         elif content == 2:
             return illust_all_data_R18
 
-    '''def download_thumbnail(self, data, content):
+    def download_thumbnail(self, data, content):
         if content == 0:
             file_name = 'all'
         elif content == 1:
@@ -93,32 +93,12 @@ class collectIllustInfo():
             temp_path = old_path.split('/')
             
             temp_path.pop(-1)
+            temp_path.pop(0)
             temp_path.append(f'{self.user_id}_{file_name}_{i+1}.jpg')
 
             new_path = '/'.join(temp_path)
 
-            os.rename(old_path,new_path)'''
-    def download_thumbnail(self, data, content):
-        if content == 0:
-            file_name = 'all'
-        elif content == 1:
-            file_name = 'general'
-        elif content == 2:
-            file_name = 'R18'
-    
-        if len(data) <= 15:
-            for_range = len(data)
-        else:
-            for_range = 15
-    
-        for i in range(for_range):
-            download_id = data[i][1]
-    
-            illust_detail = self.instance.illust_detail(download_id)
-            thumbnail_url = illust_detail['illust']['image_urls']['square_medium']
-    
-            save_path = f'/tmp/{self.user_id}_{file_name}_{i+1}.jpg'
-            self.instance.download(thumbnail_url, save_path)
+            os.rename(old_path,new_path)
 
     
     def get_maker(self, user_id):
